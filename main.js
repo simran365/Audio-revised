c6b386f735b7eff86fb938219a5192db48c71ecb
simran365 <101072213+simran365@users.noreply.github.com>
2022-12-15 17:45:31 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/Qo7mLuGII/model.json", modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results)
+{
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+    }
+}
+       
